3fc3fbbd91d46b915dca677bf33b6f3f09e38525
takuowake <takuowake@gmail.com>
2024-02-28 17:30:35 +0900

    データの保存・取得のビジネスロジックを定義

--- lib/models/todo_list.dart
+++ lib/models/todo_list.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo_app/models/todo.dart';
 
 class TodoList {
@@ -13,5 +16,22 @@ class TodoList {
   /// 指定されたidのToDoをリストから削除
   void deleteTodo(int id) {
     todos.removeWhere((todo) => todo.id == id);
+  }
+
+  /// データの保存
+  Future<void> saveTodos() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String encodedData = jsonEncode(todos.map((todo) => todo.toJson()).toList());
+    await prefs.setString('todoList', encodedData);
+  }
+
+  /// shared_preferenceからデータを取得
+  Future<void> loadTodos() async {
+    final prefs = await SharedPreferences.getInstance();
+    final String? encodedData = prefs.getString('todoList');
+    if (encodedData != null) {
+      final List<dynamic> decodedData = jsonDecode(encodedData);
+      todos = decodedData.map((item) => Todo.fromJson(item)).toList();
+    }
   }
 }
