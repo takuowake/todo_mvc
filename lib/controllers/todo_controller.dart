26248b166a1b41f3f0fa10975dc591bda270231c
takuowake <takuowake@gmail.com>
2024-02-28 17:49:04 +0900

    ControllerのDisposeを削除

--- lib/controllers/todo_controller.dart
+++ lib/controllers/todo_controller.dart
@@ -3,10 +3,6 @@ import 'package:todo_app/models/todo_list.dart';
 class TodoController {
   final TodoList todoList = TodoList();
 
-  void dispose() {
-    // 現在は何も解放するリソースがないので空
-  }
-
   void addTodo(String title) {
     todoList.addTodo(title);
   }
